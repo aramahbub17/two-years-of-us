51d6a316f842960cde5f78be34751ebdb18ebafa
aramahbub17 <mahbubalfa123@gmail.com>
2026-09-10 15:50:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
-const enterBtn = document.getElementById("enterBtn");
+const pin = document.getElementById("pin");
+const enter = document.getElementById("enter");
+const dots = document.querySelectorAll(".dots span");
 
-enterBtn.addEventListener("click", () => {
+const correctPassword = "091721";
 
-    document.body.style.transition = "opacity 1s ease";
-    document.body.style.opacity = "0";
 
-    setTimeout(() => {
-        window.location.href = "welcome.html";
-    }, 1000);
+/* Number input */
+
+pin.addEventListener("input", () => {
+
+    pin.value = pin.value.replace(/\D/g, "");
+
+    dots.forEach((dot, index) => {
+
+        if (index < pin.value.length) {
+            dot.classList.add("active");
+        } else {
+            dot.classList.remove("active");
+        }
+
+    });
 
 });
+
+
+/* Enter */
+
+enter.addEventListener("click", checkPassword);
+
+
+/* Keyboard Enter */
+
+pin.addEventListener("keydown", (event) => {
+
+    if (event.key === "Enter") {
+        checkPassword();
+    }
+
+});
+
+
+function checkPassword() {
+
+    if (pin.value === correctPassword) {
+
+        // Unlock
+        document.body.style.transition =
+            "opacity 1.2s ease";
+
+        document.body.style.opacity = "0";
+
+        setTimeout(() => {
+
+            window.location.href = "unlocked.html";
+
+        }, 1200);
+
+    } else {
+
+        // Wrong password
+        window.location.href = "error.html";
+
+    }
+
+}
